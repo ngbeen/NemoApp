9d5797ea9b35970c77cc38f6ee43372bcb361bab
ngbeen <44664457+ngbeen@users.noreply.github.com>
2023-05-25 14:17:42 +0900

    Initial Commit

---
+++ nemo/nemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  nemoApp.swift
+//  nemo
+//
+//  Created by been on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct nemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
